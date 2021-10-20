56b5194108d72f4ab6226627e1d955224894ef04
PrincessNymeria <92805969+PrincessNymeria@users.noreply.github.com>
2021-10-20 20:16:51 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
 def primary():
   print("Keep it logically awesome.")
-
+  
+  last = 13
+  rnd = random.randint(0, last)
+  
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
-  print(quotes[-1])
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
